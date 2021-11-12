3811d7afc5b5454ad0e60c70ed194621aa93c553
Rümeysa <rumeysakaragoz18@hotmail.com>
2021-11-13 01:05:45 +0300

    hatalı

---
+++ routes/artists.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const Artist = require('../models/artist')
+
+//All Artşst Route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const artists = await Artist.find(searchOptions)
+        res.render('artists/index', {
+            artists: artists,
+            searchOptions: req.query
+        })
+  } catch {
+    res.redirect('/')
+    }
+})
+
+
+//New Arstist Route
+router.get('/new', (req, res) => {
+    res.render('artists/new', { artist: new Artist() })
+  })
+
+//Create Artist Route
+router.post('/', async (req, res) => {
+    const artist = new Artist({
+        name: req.body.name
+    })
+    try{
+        const newArtist = await artist.save()
+        res.redirect(`artists`)
+    } catch{
+
+    res.render('artists/new', {            artist: artist,
+            errorMessage: "Error"
+        })
+    }
+   
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,21 +5,25 @@ if(process.env.NODE_ENV !== "production"){
 const express= require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require("body-parser")
 
 const indexRouter = require("./routes/index")
+const artistRouter = require("./routes/artists")
 
 app.set("view engine" , "ejs")
 app.set("views", __dirname + "/views")
 app.set("layout", "layouts/layout")
-app.use(expressLayouts)
+app.use(expressLayouts) 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }))
 
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
-db.on("error" , error => console.error(eroor))
+db.on("error" , error => console.error(error))
 db.once("open" , () => console.log("Connected to Mongoose"))
 
 app.use("/", indexRouter)
+app.use("/artists", artistRouter)//every route inside artistRouter will be pre pended with artists
 
 app.listen(process.env.PORT || 3000)
