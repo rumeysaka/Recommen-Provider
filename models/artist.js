d54b58f805654ce9fcfd3adb31d2d2a2ac167ca2
Rümeysa <rumeysakaragoz18@hotmail.com>
2021-11-24 01:04:39 +0300

    Finished routes

--- models/artist.js
+++ models/artist.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+const Track = require("./track")
 const artistSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -7,4 +7,15 @@ const artistSchema = new mongoose.Schema({
     }
 })
 
+artistSchema.pre('remove', function(next) {
+    Track.find({ artist: this.id }, (err, track) => {
+      if (err) {
+        next(err)
+      } else if (track.length > 0) {
+        next(new Error('This still'))
+      } else {
+        next()
+      }
+    })
+  })
 module.exports= mongoose.model("Artist", artistSchema)
--- routes/artists.js
+++ routes/artists.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Artist = require('../models/artist')
+const Track = require("../models/track")
 
 //All Artşst Route
 router.get('/', async (req, res) => {
@@ -34,7 +35,7 @@ router.post('/', async (req, res) => {
     })
     try {
         const newArtist = await artist.save()
-        res.redirect(`artists`)
+        res.redirect(`/artists/${artist.id}`)
     } catch {
 
         res.render('artists/new', {
@@ -44,5 +45,60 @@ router.post('/', async (req, res) => {
     }
 
 })
+router.get('/:id', async (req, res) => {
+    try {
+      const artist = await Artist.findById(req.params.id)
+      const tracks = await Track.find({ artist: artist.id }).limit(6).exec()
+      res.render('artists/show', {
+        artist: artist,
+        tracksByArtist: tracks
+      })
+    } catch {
+      res.redirect('/')
+    }
+  })
 
-module.exports = router
+router.get('/:id/edit', async (req, res) => {
+
+    try{
+        const artist = await Artist.findById(req.params.id)
+        res.render('artists/edit', { artist: artist })
+        } catch{
+        res.redirect("/artists")
+        }
+    })
+
+router.put('/:id', async (req, res) => {        
+    let artist   
+    try {
+        artist = await Artist.findById(req.params.id)
+        artist.name = req.body.name
+        await artist.save()
+        res.redirect(`/artists/${artist.id}`)
+    } catch {
+        if(artist == null){
+            res.redirect("/")
+        }else{
+        res.render('artists/new', {
+        artist: artist,
+        errorMessage: 'Error updating artist'
+        })
+    }
+}
+})
+
+    router.delete('/:id', async (req, res) => {
+        let artist
+        try {
+        artist = await Artist.findById(req.params.id)
+        await artist.remove()
+        res.redirect('/artists')
+        } catch {
+        if (artist == null) {
+            res.redirect('/')
+        } else {
+            res.redirect(`/artists/${artist.id}`)
+        }
+        }
+    })
+  module.exports = router
